520485820621eb49546df9a5c833379f2395f890
SaniagoRodriguezBaldovino <34890532+SaniagoRodriguezBaldovino@users.noreply.github.com>
2017-12-28 09:27:32 -0500

    First Commit

---
+++ POO.java
@@ -0,0 +1,23 @@
+
+package poo;
+
+
+public class POO {
+
+    public static void main(String[] args) { 
+   Carro c1 = new Carro(4,"Blanco"," Chevrolet");
+        System.out.println("Marca del carro:"+c1.marca);
+        System.out.println( " Color" +c1.color);
+        
+        Carro c2 = new Carro(4,"Gris"," Chevrolet");
+        System.out.println("Color" + c2.color);
+        System.out.println("Marca" + c2.marca);
+        
+        Carro c4 = new Carro(12," Azul", " Kia");
+        System.out.println("Color" + c4.color);
+        System.out.println("Marca:" + c4.marca);
+     
+    
+    } 
+}
+      
